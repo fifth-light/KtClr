33db4b953514b4fb69f70a58775c57894cd357c7
fifth_light <liu-yu-jie@foxmail.com>
2026-04-17 03:58:40 +0800

    Add ECMA-335 spec references to MethodVisitor

--- asmnet/MethodVisitor.kt
+++ asmnet/MethodVisitor.kt
@@ -1,19 +1,37 @@
 package top.fifthlight.asmnet
 
 interface MethodVisitor {
+    // ECMA-335 II.15.4.1
     fun visitMaxStack(maxStack: Int)
+
+    // ECMA-335 II.15.4.1.3
     fun visitLocalVariables(init: Boolean, locals: List<LocalVariable>)
 
     fun visitCode()
 
+    // ECMA-335 II.5.4
     fun visitLabel(label: Label)
 
+    // ECMA-335 VI.C.4.2
     fun visitInsn(opCode: OpCode)
+
+    // ECMA-335 VI.C.4.4-6, VI.C.4.11
     fun visitLdc(value: Any)
+
+    // ECMA-335 VI.C.4.3
     fun visitVarInsn(opcode: OpCode, varIndex: Int)
+
+    // ECMA-335 VI.C.4.8
     fun visitMethodInsn(opcode: OpCode, ref: MethodReference)
+
+    // ECMA-335 VI.C.4.9
     fun visitFieldInsn(opcode: OpCode, ref: FieldReference)
+
+    // ECMA-335 VI.C.4.7
     fun visitJumpInsn(opcode: OpCode, label: Label)
+
+    // ECMA-335 VI.C.4.10
     fun visitTypeInsn(opcode: OpCode, type: TypeSpec)
+
     fun visitEnd()
 }
